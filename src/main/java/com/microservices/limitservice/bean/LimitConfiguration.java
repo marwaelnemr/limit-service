bb06a157efceb00ba1fee347ad1fb42cb7ec400a
marwaelnemr <50782584+marwaelnemr@users.noreply.github.com>
2024-09-18 22:50:11 +0300

    Add files via upload

---
+++ src/main/java/com/microservices/limitservice/bean/LimitConfiguration.java
@@ -0,0 +1,41 @@
+package com.microservices.limitservice.bean;
+
+public class LimitConfiguration {
+	private int minimum;
+	private int maximum;
+	
+	
+	
+	
+	
+	protected LimitConfiguration() {
+		super();
+	}
+
+
+
+
+
+	public LimitConfiguration(int minimum, int maximum) {
+		super();
+		this.minimum = minimum;
+		this.maximum = maximum;
+	}
+
+
+
+
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+
+
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	
+}
